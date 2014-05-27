64ba680f60c9a190fb5228a19eb85d40a1979210
Qantas94Heavy <qantas94heavy@gmail.com>
2014-05-27 13:21:00 +0800

    Self-evaluation work

--- js/code.js
+++ js/code.js
@@ -2,8 +2,10 @@ function getPage() {
   var hash = location.hash.slice(1);
   if (!hash) hash = 'home';
   if (hash === 'home') location.hash = '';
-  $('.content').load('/eportfolio/pages/' + hash + '.html', function () {
-    $('.header li').removeClass('active').find('a[href="#' + hash + '"]').parent().addClass('active');
+  $('.content').load('eportfolio/pages/' + hash + '.html', function () {
+    var buttons = $('.header li');
+    if (buttons.find('a[href="#' + hash + '"]').parent().parent().hasClass(
+    buttons.removeClass('active').find('a[href="#' + hash + '"]').parent().addClass('active');
   });
 }
 
