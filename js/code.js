ea063705745f12a5d20e08919132dfdb3f8cbd12
Qantas94Heavy <qantas94heavy@gmail.com>
2014-05-29 15:29:00 +0800

    Fix files

--- js/code.js
+++ js/code.js
@@ -2,12 +2,6 @@ function getPage() {
   var hash = location.hash.slice(1);
   if (!hash) hash = 'home';
   if (hash === 'home') location.hash = '';
-  $('.content').load('pages/' + hash + '.html', function () {
-    var buttons = $('.header li');
-    var a = buttons.find('a[href="#' + hash + '"]').parent().parent();
-    if (a.hasClass('dropdown-menu')) a.addClass('active');
-    else buttons.removeClass('active').find('a[href="#' + hash + '"]').parent().addClass('active');
-  });
 }
 
 getPage();
